05ab23c31eabedf7c00c4183d05c1a6bad649532
hyurai <yuumanagai0504@icloud.com>
2021-08-04 19:28:22 +0900

    @slot使ってみた

--- web/resources/views/components/contents/content.blade.php
+++ web/resources/views/components/contents/content.blade.php
@@ -1,19 +1,13 @@
-@extends('layouts.content')
-@section('content')
-<div class = "content_main" style = "width:100%;">
-  @component('components.contents.head')
-  @endcomponent
-  <div style = "display:flex;">
-    @component('components.contents.side')
-    @endcomponent
-    
-    <div class = "content_content" style = "width:65%;">
+<div class = "content_content" style = "width:65%;">
+      
+      @foreach($tweets as $tweet)
+      
         @if(isset( $tweet->title ))
-        <div style = "border: solid 3px #000000;padding:20px;margin-right:120px;margin-left:120px;" style = "margin-bottom:20px;">
+        <div style = "border: solid 3px #000000;padding:20px;margin-right:120px;margin-left:120px;margin-bottom:20px;">
           <div style = "width:100%;display:flex;">
           <div class = "top-infomation" style = "width:10%;">
             <div style = "width:50px;height:50px;">
-              <img src="https://ferret.akamaized.net/uploads/article/6845/eyecatch/default-95e77d8922603c5a64085258c0cc3f96.png" style = "width:100%;height:100%;border-radius:50%;">
+              <img src="{{$profiles->where('user_id',$tweet->user_id)->first()->front_img}}" style = "width:100%;height:100%;border-radius:50%;">
             </div>
           </div>
           <div class = "back-infomation" style = "width:90%;">
@@ -40,10 +34,24 @@
               </div>
             </div>
             <p>{{$tweet->text}}</p>
+
+            <div class = "skills" style = "display:flex;">
+            @foreach($tweet->tweet_skills as $skill)
+              <p>{{$skill->name}}</p>
+            @endforeach
+            </div>
+
+            <div class = "hastags" style = "display:flex;">
+            @foreach($tweet->tweet_hashtag as $hashtag)
+              <form action="/hashtag/{{$hashtag->id}}" method = "get">
+                <input type="submit" value = "#{{$hashtag->hastag_name}}" style = "border: none;color: rgb(27, 149, 224);">
+              </form>
+            @endforeach
+            </div>
             <div class = "back-function" style = "display: flex;">
                @auth
-               @if($likes->where('user_id',Auth::id())->where('tweet_id',$tweet->id)->first())
-                 <form action="/tweet/like/{{$likes->where('user_id',Auth::id())->where('tweet_id',$tweet->id)->first()->id}}" method = "post" style = "width:33%;">
+               @if($tweet->liked(Auth::id(),$tweet->id))
+                 <form action="/tweet/like/{{$tweet->liked(Auth::id(),$tweet->id)->id}}" method = "post" style = "width:33%;">
                    @csrf
                    @method('DELETE')
                    <input type="submit" value = "&#xf004;" style = "font-family: FontAwesome;color:red;border: none;">
@@ -56,7 +64,7 @@
                    <input type="submit" value = "&#xf004;" style = "font-family: FontAwesome;border: none;">
                  </form>
                @endif
-               <a href="" style = "width:34%;" class = "js-modal-open-comment" data-target="modal-comment"><i class="fas fa-comments"></i></a>
+               <a href="/tweet/{{$tweet->id}}" style = "width:34%;"><i class="fas fa-comments"></i></a>
                @if($tweet->user_id == Auth::id())
                  <form action="/tweet/{{$tweet->id}}" method = "post" style = "width:33%;">
                    @csrf
@@ -76,17 +84,31 @@
           <div style = "width:100%;display:flex;">
           <div class = "top-infomation" style = "width:10%;">
             <div style = "width:50px;height:50px;">
-              <img src="{{$profiles->where('user_id',Auth::id())->first()->front_img}}" style = "width:100%;height:100%;border-radius:50%;">
+              <img src="{{$profiles->where('user_id',$tweet->user_id)->first()->front_img}}" style = "width:100%;height:100%;border-radius:50%;">
             </div>
           </div>
           <div class = "back-infomation" style = "width:90%;">
             <p style = "margin-top:0px;font-weight:bold;">{{$tweet->user->name}}</p>
             
             <p>{{$tweet->text}}</p>
+
+            <div style = "display:flex;">
+            @foreach($tweet->tweet_skills as $skill)
+              <p>{{$skill->name}}</p>
+            @endforeach
+            </div>
+            
+            <div style = "display:flex;">
+            @foreach($tweet->tweet_hashtag as $hashtag)
+              <form action="/hashtag/{{$hashtag->id}}" method = "get">
+                <input type="submit" value = "#{{$hashtag->hastag_name}}" style = "border: none;color: rgb(27, 149, 224);">
+              </form>
+            @endforeach
+            </div>
             <div class = "back-function" style = "display: flex;">
                @auth
-               @if($likes->where('user_id',Auth::id())->where('tweet_id',$tweet->id)->first())
-                 <form action="/tweet/like/{{$likes->where('user_id',Auth::id())->where('tweet_id',$tweet->id)->first()->id}}" method = "post" style = "width:33%;">
+               @if($tweet->liked(Auth::id(),$tweet->id))
+                 <form action="/tweet/like/{{$tweet->liked(Auth::id(),$tweet->id)}}" method = "post" style = "width:33%;">
                    @csrf
                    @method('DELETE')
                    <input type="submit" value = "&#xf004;" style = "font-family: FontAwesome;color:red;border: none;">
@@ -99,7 +121,7 @@
                    <input type="submit" value = "&#xf004;" style = "font-family: FontAwesome;border: none;">
                  </form>
                @endif
-               <a href="" style = "width:34%;" class = "js-modal-open-comment" data-target="modal-comment"><i class="fas fa-comments"></i></a>
+               <a href="/tweet/{{$tweet->id}}" style = "width:34%;"><i class="fas fa-comments"></i></a>
                @if($tweet->user_id == Auth::id())
                  <form action="/tweet/{{$tweet->id}}" method = "post" style = "width:33%;">
                    @csrf
@@ -115,61 +137,5 @@
           </div>
         </div>
         @endif
-
-        <div id = "modal-comment" class = "c-modal-comment js-modal-comment">
-        <div class="c-modal_bg-comment js-modal-close-comment"></div>
-        <div class = "c-modal_content-comment _lg-comment">
-          <div class = "c-modal_content_inner-comment">
-            <a class = "js-modal-close-comment c-modal_close-comment" href=""><span>閉じる</span></a>
-            <form action="/tweet/comment" method = "post">
-              @csrf
-              <input type="hidden" name = "user_id" value = "{{Auth::id()}}">
-              <input type="hidden" name = "tweet_id" value = "{{$tweet->id}}">
-              <input type="text" name = "comment">
-              <input type="submit" value = "追加">
-            </form>
-          </div>
-        </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        @foreach($comments as $comment)
-        <div style = "border: solid 3px #000000;padding:20px;margin-right:120px;margin-left:120px;" style = "margin-bottom:20px;">
-          <div style = "width:100%;display:flex;">
-          <div class = "top-infomation" style = "width:10%;">
-            <div style = "width:50px;height:50px;">
-              <img src="https://ferret.akamaized.net/uploads/article/6845/eyecatch/default-95e77d8922603c5a64085258c0cc3f96.png" style = "width:100%;height:100%;border-radius:50%;">
-            </div>
-          </div>
-          <div class = "back-infomation" style = "width:90%;">
-            <p style = "margin-top:0px;font-weight:bold;">{{$tweet->user->name}}</p>            
-            <p>{{$comment->comment}}</p>
-            <div class = "back-function" style = "display: flex;">
-            </div>
-          </div>
-          </div>
-        </div>
-        @endforeach
+      @endforeach
     </div>
-  </div>
-</div>
-</div>
-@endsection
